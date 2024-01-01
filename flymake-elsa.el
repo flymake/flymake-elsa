ae7bd4b8077983523431a587619c9e38a62f4312
Jen-Chieh Shen <jcs090218@gmail.com>
2024-01-01 00:37:21 -0800

    chore: Update copyright year

--- flymake-elsa.el
+++ flymake-elsa.el
@@ -1,6 +1,6 @@
 ;;; flymake-elsa.el --- Flymake integration for Elsa  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023  Shen, Jen-Chieh
+;; Copyright (C) 2023-2024  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
